bf6a2343e93127300822b99c8d6a2ca197e39f9a
CarlosDev28 <carlos.pinto@enaccion-c.cl>
2025-02-27 17:13:17 -0300

    iniciando en git

--- backend/index.js
+++ backend/index.js
@@ -113,7 +113,7 @@ app.get('/comandas', async (req, res) => {
         c.numero_mesa, 
         c.cantidad, 
         c.detalles,
-        c.estado
+        c.estados
       FROM 
         comanda c
       JOIN 
